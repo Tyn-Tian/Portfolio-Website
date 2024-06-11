931073052ddefa33aa9f38f4b1ba015ea852f4ab
Christian <kristian2015ok@gmail.com>
2024-06-11 08:27:04 +0700

    Perf: skill page animation & icon

--- src/data/skill.js
+++ src/data/skill.js
@@ -1,53 +1,70 @@
 export const skills = [
   {
+    url: "https://cdn.iconscout.com/icon/premium/png-256-thumb/html-2752158-2284975.png?f=webp",
     name: "HTML",
   },
   {
+    url: "https://cdn.iconscout.com/icon/free/png-256/free-css-alt-3628710-3029935.png",
     name: "CSS",
   },
   {
+    url: "https://cdn.iconscout.com/icon/free/png-256/free-sass-3629037-3030394.png",
     name: "Sass",
   },
   {
+    url: "https://cdn.iconscout.com/icon/free/png-256/free-javascript-1-225993.png?f=webp",
     name: "JavaScript",
   },
   {
+    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png",
     name: "Bootstrap",
   },
   {
+    url: "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
     name: "React Js",
   },
   {
+    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png",
     name: "Vue Js",
   },
   {
+    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2560px-PHP-logo.svg.png",
     name: "PHP",
   },
   {
+    url: "https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png",
     name: "Node Js",
   },
   {
+    url: "https://upload.wikimedia.org/wikipedia/labs/8/8e/Mysql_logo.png",
     name: "MySQL",
   },
   {
+    url: "https://miro.medium.com/v2/resize:fit:475/1*4ZdRWapHBpflTN5FKaDV4w.png",
     name: "REST API",
   },
   {
+    url: "https://seeklogo.com/images/O/openapi-logo-3E54DE56CD-seeklogo.com.png",
     name: "OpenAPI",
   },
   {
+    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1969px-Laravel.svg.png",
     name: "Laravel",
   },
   {
+    url: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
     name: "Git",
   },
   {
+    url: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
     name: "Github",
   },
   {
+    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/1667px-Figma-logo.svg.png",
     name: "Figma",
   },
   {
+    url: "https://cdn.iconscout.com/icon/free/png-256/free-postman-3521648-2945092.png?f=webp",
     name: "Postman",
   },
 ];
--- src/pages/Skill.js
+++ src/pages/Skill.js
@@ -1,6 +1,27 @@
 import { skills } from "../data/skill";
+import { motion } from "framer-motion";
 
 const Skill = () => {
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 0.75,
+    },
+  };
+
   return (
     <main className="mt-md-5 pt-md-5 mt-3 pt-3 px-3 px-md-5">
       <section className="skills container">
@@ -11,13 +32,23 @@ const Skill = () => {
             past 1 years.
           </p>
         </div>
-        <div className="skills-container d-flex justify-content-center flex-wrap gap-md-3 gap-2">
-          {skills.map((skill) => (
-            <div className="skill-item px-3 py-2 border rounded-3 opacity-50 fw-semibold">
+        <motion.div
+          className="skills-container d-flex justify-content-center flex-wrap gap-md-3 gap-2"
+          variants={container}
+          initial="hidden"
+          animate="visible"
+        >
+          {skills.map((skill, index) => (
+            <motion.div
+              key={index}
+              className="skill-item px-3 py-2 border rounded-3 fw-semibold d-flex gap-2 align-items-center"
+              variants={item}
+            >
+              <img src={skill.url} alt="Logo" />
               {skill.name}
-            </div>
+            </motion.div>
           ))}
-        </div>
+        </motion.div>
       </section>
     </main>
   );
