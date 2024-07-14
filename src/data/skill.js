29c4d6ff2d9985b07bd38835b970c626f32c567b
Christian <kristian2015ok@gmail.com>
2024-07-14 13:32:22 +0700

    Perf: add several skills

--- src/data/skill.js
+++ src/data/skill.js
@@ -20,8 +20,16 @@ export const skills = [
     name: "Bootstrap",
   },
   {
+    url: "https://cdn.iconscout.com/icon/free/png-256/free-jest-3521517-2945020.png?f=webp",
+    name: "Jest"
+  },
+  {
     url: "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
     name: "React Js",
+  },
+  {
+    url: "https://testing-library.com/img/octopus-128x128.png",
+    name: "Testing-library",
   },
   {
     url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png",
@@ -30,6 +38,10 @@ export const skills = [
   {
     url: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2560px-PHP-logo.svg.png",
     name: "PHP",
+  },
+  {
+    url: "https://cdn.worldvectorlogo.com/logos/phpunit.svg",
+    name: "PHPUnit",
   },
   {
     url: "https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png",
@@ -66,5 +78,13 @@ export const skills = [
   {
     url: "https://cdn.iconscout.com/icon/free/png-256/free-postman-3521648-2945092.png?f=webp",
     name: "Postman",
+  },
+  {
+    url: "https://cdn.icon-icons.com/icons2/2699/PNG/512/expressjs_logo_icon_169185.png",
+    name: "Express.js",
+  },
+  {
+    url: "https://cdn.worldvectorlogo.com/logos/prisma-4.svg",
+    name: "Prisma ORM",
   },
 ];
